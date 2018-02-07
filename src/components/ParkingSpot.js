b38878bbbc80ea0f91c73486b09893796edde88e
Hui Wang <huiw3939@gmail.com>
2018-02-06 20:23:11 -0500

    stars

--- src/components/ParkingSpot.js
+++ src/components/ParkingSpot.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const ParkingSpot = (props) => {
   const detailLink = `/parkingSpot/${props.spot.id}`
-  console.log(props)
+  const roundedRating = (Math.round(props.spot.rating*2)/2).toFixed(1)
+  console.log(roundedRating)
+  let stars = <div></div>
+  // for(let i = roundedRating; i>0; i--){
+  //   if(i === .5){
+  //     return <i className="fa fa-star-half fa-lg" aria-hidden="true"></i>
+  //   }else{
+  //     return <i className="fa fa-star fa-lg" aria-hidden="true"></i>
+  //   }
+  // }
+  // console.log(stars)
   return (
       <div className="col-sm-12 col-md-6 col-lg-4">
         <div className="card">
@@ -13,16 +23,13 @@ const ParkingSpot = (props) => {
         <small className="text-muted float-right">Type: {props.spot.type_of_space}</small>
         <h4 className="card-title">{props.spot.title}</h4>
         <div className="card-text">Price: ${props.spot.price} Per Day</div>
-        <div className="card-text">Rating: {props.spot.rating}</div>
-          <i className="fa fa-star-o" aria-hidden="true"></i>
-          <i className="fa fa-star-o" aria-hidden="true"></i>
-          <i className="fa fa-star-o" aria-hidden="true"></i>
-          <i className="fa fa-star-o" aria-hidden="true"></i>
-          <i className="fa fa-star-o" aria-hidden="true"></i>
+        <div className="card-text">Rating: {roundedRating}/5</div>
+        {stars}
+
       </div>
       </div>
     </div>
   )
 }
 
-export default ParkingSpot
+export default ParkingSpot;
