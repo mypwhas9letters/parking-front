0c701942522d6780eed309f38061f3b2f3f8a9f9
Hui Wang <huiw3939@gmail.com>
2018-02-22 18:41:17 -0500

    cleaning up

--- src/components/reservations/Reservation.js
+++ src/components/reservations/Reservation.js
@@ -1,76 +1,70 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+import noImage from '../../images/no-image.jpg';
 
 const Reservation = (props) => {
-  const profileImg = props.reservation.guest.profile_pic ? props.reservation.guest.profile_pic : "http://loyalkng.com/wp-content/uploads/2010/01/facebook-art-no-photo-image-batman-mickey-mouse-spock-elvis-rick-roll.jpg"
+  const profileImg = props.reservation.guest.profile_pic ? props.reservation.guest.profile_pic : noImage
   const statusButtons = props.reservation.status === "pending" ? (<div className="right floated content"><button name="approved" value={props.reservation.id} onClick={props.onclick} className="ui primary button">Confirm</button><button name="denied" value={props.reservation.id} onClick={props.onclick} className="ui red button">Deny</button></div>) : null
   const currentStep = props.reservation.status
   const detailLink = `/parkingSpot/${props.reservation.parking_spot.id}`
-
   return (
     <div className="card">
       <div className="content">
         {currentStep !== "denied" ?
-                        <div className="ui four steps">
-
-                          <div className="step">
-                            <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
-                            <div className="content">
-                              <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
-                              <NavLink to={detailLink}><div className="description" style={{color:"white"}}>{props.reservation.parking_spot.title}</div></NavLink>
-                              <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
-                            </div>
-                          </div>
-
-                          <div className={currentStep === "pending" ? "active step": "completed step"}>
-                            <i className="check circle icon"></i>
-                            <div className="content">
-                              <div className="title" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Your Confirmation": "Confirmed"}</div>
-                              <div className="description">{statusButtons}</div>
-
-                            </div>
-                          </div>
-                          <div className={currentStep === "approved" ? "active step": "step disabled"}>
-                            <i className="payment icon"></i>
-                            <div className="content">
-                              {currentStep === "approved" ? <div className="title" style={{color:"white"}}>Billing</div> : <div className="title">Billing</div> }
-                              {currentStep === "approved" ? <div className="description" style={{color:"white"}}>Waiting For Billing Information</div> : <div className="description">Enter billing information</div> }
-                            </div>
-                          </div>
-                          <div className="disabled step">
-                            <i className="info icon"></i>
-                            <div className="content">
-                              <div className="title">Completed</div>
-                              <div className="description">Verify order details</div>
-                            </div>
-                          </div>
-                        </div>
-                        :
-                        <div className="ui small steps">
-                        <div className="step">
-                          <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
-                          <div className="content">
-                            <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
-                            <div className="description" style={{color:"white"}}>Requested to book {props.reservation.parking_spot.title}</div>
-                            <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
-                          </div>
-                        </div>
-                        <div className="step deniedStep">
-                          <i className="remove icon"></i>
-                          <div className="content">
-                            <div className="title" style={{color:"white"}}>Denied</div>
-                            <div className="description" style={{color:"white"}}>Sorry, your request was denied</div>
-                          </div>
-                        </div>
-                      </div>
-
-                        }
-        </div>
-
-
+          <div className="ui four steps">
+            <div className="step">
+              <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
+                <NavLink to={detailLink}><div className="description" style={{color:"white"}}>{props.reservation.parking_spot.title}</div></NavLink>
+                <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
+              </div>
+            </div>
+            <div className={currentStep === "pending" ? "active step": "completed step"}>
+              <i className="check circle icon"></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Your Confirmation": "Confirmed"}</div>
+                <div className="description">{statusButtons}</div>
+              </div>
+            </div>
+            <div className={currentStep === "approved" ? "active step": "step disabled"}>
+              <i className="payment icon"></i>
+              <div className="content">
+                {currentStep === "approved" ? <div className="title" style={{color:"white"}}>Billing</div> : <div className="title">Billing</div> }
+                {currentStep === "approved" ? <div className="description" style={{color:"white"}}>Waiting For Billing Information</div> : <div className="description">Enter billing information</div> }
+              </div>
+            </div>
+            <div className="disabled step">
+              <i className="info icon"></i>
+              <div className="content">
+                <div className="title">Completed</div>
+                <div className="description">Verify order details</div>
+              </div>
+            </div>
+          </div>
+        :
+          <div className="ui small steps">
+            <div className="step">
+              <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
+                <div className="description" style={{color:"white"}}>Requested to book {props.reservation.parking_spot.title}</div>
+                <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
+              </div>
+            </div>
+            <div className="step deniedStep">
+              <i className="remove icon"></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>Denied</div>
+                <div className="description" style={{color:"white"}}>Sorry, your request was denied</div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   )
 }
 
-export default Reservation
+export default Reservation;
--- src/components/reservations/YourReservation.js
+++ src/components/reservations/YourReservation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import noImage from '../../images/no-image.jpg';
 
 const YourReservation = (props) => {
-  const profileImg = props.reservation.guest.profile_pic ? props.reservation.guest.profile_pic : "http://loyalkng.com/wp-content/uploads/2010/01/facebook-art-no-photo-image-batman-mickey-mouse-spock-elvis-rick-roll.jpg"
+  const profileImg = props.reservation.guest.profile_pic ? props.reservation.guest.profile_pic : noImage
   // const statusButtons = props.reservation.status === "pending" ? (<div><button name="approved" value={props.reservation.id} className="ui primary button">Confirm</button><button name="denied" value={props.reservation.id} onClick={props.onclick} className="ui red button">Deny</button></div>) : null
   // const YourReservationStatus = props.reservation.status.charAt(0).toUpperCase() + props.reservation.status.slice(1)
   const currentStep = props.reservation.status
@@ -11,10 +12,8 @@ const YourReservation = (props) => {
   return (
     <div className="card">
       <div className="content">
-
         {currentStep !== "denied" ?
           <div className="ui four steps">
-
             <div className="step">
               <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
               <div className="content">
@@ -23,54 +22,48 @@ const YourReservation = (props) => {
                 <div className="description"><p className="whiteText">Date {props.reservation.date}</p></div>
               </div>
             </div>
-
-
-
-                          <div className={currentStep === "pending" ? "active step": "completed step"}>
-                            <i className="check circle icon"></i>
-                            <div className="content">
-                              <div className="title" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Confirmation": "Confirmed"}</div>
-                              <div className="description" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Confirmation From Host": ""}</div>
-                            </div>
-                          </div>
-                          <div className={currentStep === "approved" ? "active step": "step disabled"}>
-                            <i className="payment icon"></i>
-                            <div className="content">
-                              {currentStep === "approved" ? <div className="title" style={{color:"white"}}>Billing</div> : <div className="title">Billing</div> }
-                              {currentStep === "approved" ? <div className="description" style={{color:"white"}}>Enter billing information</div> : <div className="description">Enter billing information</div> }
-
-                            </div>
-                          </div>
-                          <div className="step disabled">
-                            <i className="info icon"></i>
-                            <div className="content">
-                              <div className="title">Completed</div>
-                              <div className="description">Verify order details</div>
-                            </div>
-                          </div>
-                        </div>
-                        :
-
-                        <div className="ui small steps">
-                          <div className="step">
-                            <i><img className="ui avatar image" src={profileImg} alt=""/></i>
-                            <div className="content">
-                              <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
-                              <NavLink to={detailLink}><div className="description" style={{color:"white"}}>Requested to book {props.reservation.parking_spot.title}</div></NavLink>
-                              <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
-                            </div>
-                          </div>
-                        <div className="step deniedStep">
-                          <i className="remove icon"></i>
-                          <div className="content">
-                            <div className="title" style={{color:"white"}}>Denied</div>
-                            <div className="description" style={{color:"white"}}>Sorry, your request was denied</div>
-                          </div>
-                        </div>
-                        </div>
-                        }
-        </div>
-
+            <div className={currentStep === "pending" ? "active step": "completed step"}>
+              <i className="check circle icon"></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Confirmation": "Confirmed"}</div>
+                <div className="description" style={{color:"white"}}>{currentStep === "pending" ? "Awaiting Confirmation From Host": ""}</div>
+              </div>
+            </div>
+            <div className={currentStep === "approved" ? "active step": "step disabled"}>
+              <i className="payment icon"></i>
+              <div className="content">
+                {currentStep === "approved" ? <div className="title" style={{color:"white"}}>Billing</div> : <div className="title">Billing</div> }
+                {currentStep === "approved" ? <div className="description" style={{color:"white"}}>Enter billing information</div> : <div className="description">Enter billing information</div> }
+              </div>
+            </div>
+            <div className="step disabled">
+              <i className="info icon"></i>
+              <div className="content">
+                <div className="title">Completed</div>
+                <div className="description">Verify order details</div>
+              </div>
+            </div>
+          </div>
+        :
+          <div className="ui small steps">
+            <div className="step">
+              <i><img className="ui avatar image" src={profileImg} width="100px" alt=""/></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>{props.reservation.guest.username}</div>
+                <NavLink to={detailLink}><div className="description" style={{color:"white"}}>Requested to book {props.reservation.parking_spot.title}</div></NavLink>
+                <div className="description" style={{color:"white"}}>Date {props.reservation.date}</div>
+              </div>
+            </div>
+            <div className="step deniedStep">
+              <i className="remove icon"></i>
+              <div className="content">
+                <div className="title" style={{color:"white"}}>Denied</div>
+                <div className="description" style={{color:"white"}}>Sorry, your request was denied</div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   )
 }
